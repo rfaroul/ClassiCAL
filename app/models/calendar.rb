a397e2905b2a6aea67f13d5569e85aa0a22649eb
Rachelle Faroul <rachelle.faroul@gmail.com>
2015-08-12 09:09:39 -0400

    models

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -5,3 +5,6 @@ end
 
 #A 'belongs_to' association sets up a one-to-one connection with another model, such that each instance of the declaring model "belongs to" one instance of the other model.
 
+def add_course(course) 
+	self.
+end
