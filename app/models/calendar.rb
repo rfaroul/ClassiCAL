a848c9be6c8ee1a83c15636fa8fe00ec6d20dc82
Rachelle Faroul <rachelle.faroul@gmail.com>
2015-08-12 09:08:30 -0400

    rails backend

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -4,3 +4,4 @@ class Calendar < ActiveRecord::Base
 end
 
 #A 'belongs_to' association sets up a one-to-one connection with another model, such that each instance of the declaring model "belongs to" one instance of the other model.
+
